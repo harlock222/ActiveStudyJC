e1546c1a8b7bbefb8e77e08268664e42ccee1b23
harlock222 <kieuthanh2158@gmail.com>
2016-06-10 00:08:59 +0700

    MyQueue

--- src/Day11/CheckFile.java
+++ src/Day11/CheckFile.java
@@ -8,16 +8,11 @@ package Day11;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
-import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.BlockingQueue;
-import java.util.logging.Level;
 import org.apache.log4j.Logger;
 
 /**
